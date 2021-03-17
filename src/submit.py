24fcd3d79ee1c47d7f233be6d963fb93e7363ecc
CodeSEAS <manjun@seas.upenn.edu>
2021-03-17 00:24:48 -0400

    updated py files

---
+++ src/submit.py
@@ -0,0 +1,8 @@
+def main():
+    print('Enter the file you are going to submit: (example - test.py)')
+    file_name = input()
+    print('Checking ... ' + file_name)
+
+
+if __name__ == '__main__':
+    main()
